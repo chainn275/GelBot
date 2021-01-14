042147aa53ebf520f53c4919283851f2bbb39c13
SzyKania <72377599+SzyKania@users.noreply.github.com>
2021-01-14 22:50:17 +0100

    Update notes.py

--- notes.py
+++ notes.py
@@ -3,7 +3,7 @@ I do not own the pictures, I simply provide for you a connection
 from your Discord server to Gelbooru using both legal and free APIs.
 
 I am not responsible in any way for the content that GelBot will send to
-the chat. Keep in mind YOU are the one who type the tags.
+the chat. Keep in mind YOU are the one typing the tags.
 """
 
 help = """
@@ -12,7 +12,7 @@ get pictures. !pic is what you need. Syntax:
 
 !pic [optional rating] tag/tag with a space/tag/tag
 
-Type !gelexamples to get better grasp of the command.
+Type !gelexamples to get a better grasp of the command.
 
 Type !gelratings if you don't know how to use ratings.
 
@@ -34,7 +34,7 @@ rs - rating safe - mostly SFW content,
 rq - rating questionable - mostly NSFW, well questionable content,
 re - rating explicit - definitely unquestionable NSFW content.
 
-For both rating questionable and explicit NSFW channel is required.
+For questionable and explicit ratings an NSFW channel is required.
 
-If you don't specify any, GelBot will look only within rating safe.
+If you don't specify any, GelBot will only look for safe rated pics.
 """
